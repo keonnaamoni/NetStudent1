664b2f918adb7d2480b052b485b5e207e94f9bec
keonnaamoni <keonnaevans@gmail.com>
2015-03-27 20:32:44 -0400

    NetStudent added class

---
+++ NetStudent1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetStudent1
+{
+    class Student
+    {
+        public int points;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Keonna = new Student();
+            Keonna.points = 0; 
+            Console.WriteLine ("Keonna's beginning points is {0}", Keonna.points);
+        }
+    }
+}
