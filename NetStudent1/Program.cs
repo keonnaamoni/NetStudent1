903f84348511a7d517da195432b6cd3f92ed2f0a
keonnaamoni <keonnaevans@gmail.com>
2015-03-28 18:45:00 -0400

    NetStudent

--- NetStudent1/Program.cs
+++ NetStudent1/Program.cs
@@ -6,18 +6,49 @@ using System.Threading.Tasks;
 
 namespace NetStudent1
 {
-    class Student
-    {
-        public int points;
-    }
-
     class Program
     {
+
+        enum Rank
+        {
+            Grasshopper,
+            Journeyman,
+            RockStar,
+            Ninja,
+            Jedi
+        }
+    
         static void Main(string[] args)
         {
-            Student Keonna = new Student();
-            Keonna.points = 0; 
-            Console.WriteLine ("Keonna's beginning points is {0}", Keonna.points);
+            Student keonnaEvans = new Student();
+            
+            keonnaEvans.CompletedProgram = + 1;
+            keonnaEvans.Helper = + 2;
+            keonnaEvans.Level = null;
+
+            
+            
+
+            Console.WriteLine("Did you help a student today?");
+            string line = Console.ReadLine();
+          
+           
+            Console.WriteLine(keonnaEvans.Helper + keonnaEvans.CompletedProgram);            
         }
+        
+                
+
+        
+    
+    }
+    
+
+    class Student
+    {
+        public int CompletedProgram { get; set; }
+        public int Helper { get; set; }
+        public string Level { get; set; }
+        
+
     }
 }
